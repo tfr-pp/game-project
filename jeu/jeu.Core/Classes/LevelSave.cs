ffb697f91885bea1a83763b6990632a10b2cf9d0
Karim <karim.rolle7@gmail.com>
2025-11-28 01:44:10 +0100

    LevelSave et SaveManager

---
+++ jeu/jeu.Core/Classes/LevelSave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace jeu.Core.Classes;
+
+public class LevelSave
+{
+    [XmlAttribute] public int Id { get; set; }
+    [XmlAttribute] public bool Completed { get; set; }
+}
+
+[XmlRoot("SaveLevels")]
+public class LevelsSave
+{
+    [XmlElement("Level")]
+    public List<LevelSave> Levels { get; set; } = new List<LevelSave>();
+}
---
+++ jeu/jeu.Core/Classes/SaveManager.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace jeu.Core.Classes;
+
+public class SaveManager
+{
+    private string _folder = "Saves";
+    private string _file => Path.Combine(_folder, "SaveLevels.xml");
+
+    public SaveManager()
+    {
+        if (!Directory.Exists(_folder))
+            Directory.CreateDirectory(_folder);
+    }
+
+    public LevelsSave LoadLevels()
+    {
+        if (!File.Exists(_file))
+            return new LevelsSave();
+        
+        XmlSerializer serializer = new XmlSerializer(typeof(LevelsSave));
+        using (FileStream stream = File.OpenRead(_file))
+            return (LevelsSave)serializer.Deserialize(stream);
+    }
+
+    public void CompleteLevel(int levelId)
+    {
+        LevelsSave save = LoadLevels();
+        LevelSave level = save.Levels.Find(l => l.Id == levelId);
+        if (level == null)
+        {
+            level = new LevelSave { Id = levelId, Completed = true };
+            save.Levels.Add(level);
+        }
+        else
+            level.Completed = true;
+
+        XmlSerializer serializer = new XmlSerializer(typeof(LevelsSave));
+        using (FileStream stream = File.Create(_file))
+            serializer.Serialize(stream, save);
+    }
+
+    public bool IsLevelUnlocked(int levelId)
+    {
+        if (levelId == 1)
+            return true; // Pour que le premier soit toujours dispo
+        
+        LevelsSave save = LoadLevels();
+        LevelSave prev = save.Levels.Find(l => l.Id == levelId - 1);
+        return prev != null && prev.Completed;
+    }
+}
